6f0fc3ec414f59e494c4e39da7891b188f36680e
Mattia Bardino <Mattia.errol@gmail.com>
2022-01-24 00:06:10 +0100

    fatto

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const containerElement = document.getElementById("container");
 
-for (let i = 0; i <= 1000; i++) {    
+for (let i = 1; i <= 1000; i++) {    
     
-    if (i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 != 0) {
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box-green'>" + "Fizz" + "</div>";
     }
 
-    else if (i % 5 == 0) {
+    else if (i % 5 == 0 && i % 3 != 0) {
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box-orange'>" + "Buzz" + "</div>";
     } 
     
-    else if ((i%3==0)&& i%5==0) {
+    else if ((i % 3 == 0) && i % 5 == 0 ) {
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box-red'>" + "FizzBuzz" + "</div>";
     } 
 
