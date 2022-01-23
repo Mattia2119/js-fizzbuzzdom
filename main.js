343bcf896a68efe107ec846fad037b1f2f9ba515
Mattia Bardino <Mattia.errol@gmail.com>
2022-01-23 01:06:44 +0100

    tentativo

---
+++ main.js
@@ -0,0 +1,22 @@
+const containerElement = document.getElementById("container");
+
+for (let i = 0; i <= 1000; i++) {    
+    
+    if (i % 3 == 0) {
+        containerElement.innerHTML = containerElement.innerHTML + "<div class='box-green'>" + "Fizz" + "</div>";
+    }
+
+    else if (i % 5 == 0) {
+        containerElement.innerHTML = containerElement.innerHTML + "<div class='box-orange'>" + "Buzz" + "</div>";
+    } 
+    
+    else if ((i%3==0)&& i%5==0) {
+        containerElement.innerHTML = containerElement.innerHTML + "<div class='box-red'>" + "FizzBuzz" + "</div>";
+    } 
+
+    else {
+        containerElement.innerHTML = containerElement.innerHTML + "<div class='box'>" + i + "</div>";
+    }
+
+
+}
